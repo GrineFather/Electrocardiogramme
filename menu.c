1684dbce3f9e5bc34928da6bc69e56752c4bd6b7
yoan92 <44927560+yoan92@users.noreply.github.com>
2018-11-12 09:18:39 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 
 int choix (int c){
-printf("1:Afficher les données dans l’ordre du fichier .csv\n");
-printf("2:Afficher les données en ordre croissant\n");
-printf("3:Afficher les données en ordre décroissant\n");
-printf("4:Afficher les données en ordre selon le temps\n");
-printf("5:Afficher les données en ordre selon le pouls\n");
-printf("6: Rechercher et afficher les données pour un temps particulier\n");
-printf("7:Afficher la moyenne de pouls dans une plage de temps donnée\n");
-printf("8:Afficher le nombre de lignes de données actuellement en mémoire\n");
-printf("9: Rechercher et afficher les max/min de pouls avec le temps associé de pi\n");
-printf("10: Quitter l’application\n");
+  
+printf("1:Afficher les donnÃ©es dans lâ€™ordre du fichier .csv\n");
+printf("2:Afficher les donnÃ©es en ordre croissant\n");
+printf("3:Afficher les donnÃ©es en ordre dÃ©croissant\n");
+printf("4:Afficher les donnÃ©es en ordre selon le temps\n");
+printf("5:Afficher les donnÃ©es en ordre selon le pouls\n");
+printf("6: Rechercher et afficher les donnÃ©es pour un temps particulier\n");
+printf("7:Afficher la moyenne de pouls dans une plage de temps donnÃ©e\n");
+printf("8:Afficher le nombre de lignes de donnÃ©es actuellement en mÃ©moire\n");
+printf("9: Rechercher et afficher les max/min de pouls avec le temps associÃ© de pi\n");
+printf("10: Quitter lâ€™application\n");
                 if (c==1){
                     }
 
