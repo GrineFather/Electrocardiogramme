9880d5b6dd9ab0e2cd10d367955008e9a102ee8a
GrineFather <44920911+GrineFather@users.noreply.github.com>
2018-11-12 11:31:55 +0100

    Update actions.c

--- actions.c
+++ actions.c
@@ -23,3 +23,18 @@ void quick_sort(int tab[],int deb,int fin)
  tri_tab_recursif(tab,deb,pos);
  tri_tab_recursif(tab,pos+1,fin);
 }
+
+void search(int n, struct inhabitant *a){
+    char prenomsearch[MAX_STR_SIZE];
+    char nomsearch[MAX_STR_SIZE];
+    printf("Prenom et nom a chercher : \n");
+    scanf("%s%s", prenomsearch, nomsearch);
+    for (int i=0; i<n; i++)
+    {
+        if (strcmp(a[i].prenom, prenomsearch)==0 && strcmp(a[i].nom, nomsearch)==0)
+        {
+            printf("%d, %s, %s, %d\n", a[i].distance, a[i].prenom,a[i].nom, a[i].zip);
+        }
+    }
+    printf("Fin de la recherche");
+}
